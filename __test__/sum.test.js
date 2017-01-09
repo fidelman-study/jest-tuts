8f2caa60e76a4a408a5741730818bf4794fcf1fb
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-09 21:52:07 +0100

    before test

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,5 +1,20 @@
 const sum = require('../js/sum');
 
-it('1 + 2 to equal 3', () => {
-	expect(sum(1,2)).toBe(3)
-})
+
+
+describe('test', function() {
+
+	beforeAll(function() {console.log('Before Test group');});
+	beforeEach(function() {console.log('Before Test');});
+
+	it('1 + 2 to equal 3', () => {
+		afterEach(() => {
+			expect(sum(1,2)).toBe(3)
+		});
+	});
+
+	afterEach(function() {console.log('After Test');});
+	afterAll(function() {console.log('after Test group');});
+
+
+});
