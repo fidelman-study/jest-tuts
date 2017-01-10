65f143596dcbf986e29581826b63f8c5e08b640a
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-10 20:57:24 +0100

    result is not undefined

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,9 +1,8 @@
 const sum = require('../js/sum.js');
-console.log(sum(0.2 + 0.1));
 
 
-describe('adding numbers', () => {
-  it('works sanely with simple decimals', () => {
-    expect(sum(0.2, 0.1)).toBeCloseTo(0.3, 5);
+describe('Defined variables', () => {
+  it('a variable is defined', () => {
+    expect(sum(0.2, 0.1)).toBeDefined();
   });
 });
