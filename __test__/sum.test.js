23eca3d0734d43b7ecf6802fdf8481f4a1562bdf
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-10 20:37:11 +0100

    not equals

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,7 +1,7 @@
 const bestLaCroixFlavor = require('../js/sum.js');
 
 describe('the best La Croix flavor', () => {
-  it('is grapefruit', () => {
-    expect(bestLaCroixFlavor()).toBe('grapefruit');
+  it('is not coconut', () => {
+    expect(bestLaCroixFlavor()).not.toBe('coconut');
   });
 });
