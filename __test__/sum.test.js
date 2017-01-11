3265af8903f01191d1513af2bef3ea5fe72253ac
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-12 00:29:06 +0100

    compare item in an array

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,17 +1,9 @@
-const can1 = {
-  flavor: 'grapefruit',
-  ounces: 12,
-};
-const can2 = {
-  flavor: 'grapefruit',
-  ounces: 12,
-};
 
-describe('the La Croix cans on my desk', () => {
-  it('have all the same properties', () => {
-    expect(can1).toEqual(can2);
+
+const canArray =  ['grapefruit', 12] ;
+
+
+
+  it('contain item in array', () => {
+    expect(canArray).toContain(12);
   });
-  it('are not the exact same can', () => {
-    expect(can1).not.toBe(can2);
-  });
-});
