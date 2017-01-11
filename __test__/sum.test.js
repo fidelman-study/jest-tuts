68a68e5bfb8814f6cd5e3bc2b1f9dd777b963ba1
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-12 00:08:56 +0100

    instance of

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,20 +1,9 @@
 const sum = require('../js/sum.js');
 
 
-describe('ounces per can', () => {
-  it('is more than 10', () => {
-    expect(11).toBeGreaterThan(10);
-  });
+class A {}
 
-  it('is at least 12', () => {
-    expect(12).toBeGreaterThanOrEqual(12);
-  });
 
-  it('is less than 5', () => {
-    expect(4).toBeLessThan(5);
-  });
-
-  it('is at least 100', () => {
-    expect(100).toBeLessThanOrEqual(100);
-  });
+it('is instance of A', () => {
+	expect(new A).toBeInstanceOf(A);
 });
