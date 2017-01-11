b0e4cb41ea357f8c223ee31733a5ea78a0b39df4
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-12 00:06:46 +0100

    more less and at least

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,8 +1,20 @@
 const sum = require('../js/sum.js');
 
 
-describe('Defined variables', () => {
-  it('a variable is defined', () => {
-    expect(sum(0.2, 0.1)).toBeDefined();
+describe('ounces per can', () => {
+  it('is more than 10', () => {
+    expect(11).toBeGreaterThan(10);
+  });
+
+  it('is at least 12', () => {
+    expect(12).toBeGreaterThanOrEqual(12);
+  });
+
+  it('is less than 5', () => {
+    expect(4).toBeLessThan(5);
+  });
+
+  it('is at least 100', () => {
+    expect(100).toBeLessThanOrEqual(100);
   });
 });
