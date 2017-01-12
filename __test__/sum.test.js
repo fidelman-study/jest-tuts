e20e2ab829f7c09416c0654e11bb6132598b83fd
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-01-12 20:14:41 +0100

    contain obj props

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,11 +1,22 @@
-describe('an essay on the best flavor', () => {
-  it('mentions grapefruit', () => {
-    expect('grapefruit').toMatch(/grapefruit/);
-  })
-})
+const houseForSale = {
+    bath: true,
+    kitchen: {
+        amenities: ['oven', 'stove', 'washer'],
+        area: 20,
+        wallColor: 'white'
+    },
+  bedrooms: 4
+};
+const desiredHouse = {
+    bath: true,
+    kitchen: {
+        amenities: ['oven', 'stove', 'washer'],
+        wallColor: 'white'
+    }
+};
 
-describe('grapefruits are healthy', () => {
-  it('grapefruits are a fruit', () => {
-    expect('grapefruits').toMatch('fruit');
-  })
-})
+describe('looking for a new house', () => {
+    it('the house has my desired features', () => {
+        expect(houseForSale).toMatchObject(desiredHouse);
+    });
+});
